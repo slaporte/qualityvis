09b0e4ce3773ab03aa7f6b12530590a790564b5f
Mahmoud Hashemi <makuro@gmail.com>
2012-08-19 23:00:55 -0700

    PEP 8

--- stats.py
+++ stats.py
@@ -1,34 +1,42 @@
 def mean(vals):
     return sum(vals) / len(vals)
 
+
 def median(vals):
     copy = sorted(vals)
     size = len(vals)
     if size % 2 == 1:
         return copy[(size - 1) / 2]
     else:
-        return (copy[size/2 - 1] + copy[size/2]) / 2
+        return (copy[size / 2 - 1] + copy[size / 2]) / 2
+
 
 def pow_diff(vals, power):
     m = mean(vals)
     return [(v - m) ** power for v in vals]
 
+
 def variance(vals):
     return mean(pow_diff(vals, 2))
+
 
 def std_dev(vals):
     return variance(vals) ** 0.5
 
+
 def rel_std_dev(vals):
     return std_dev(vals) / mean(vals)
 
+
 def skewness(vals):
-    return (sum(pow_diff(vals, 3)) / 
+    return (sum(pow_diff(vals, 3)) /
             ((len(vals) - 1) * (std_dev(vals) ** 3)))
 
+
 def kurtosis(vals):
-    return (sum(pow_diff(vals, 4)) / 
+    return (sum(pow_diff(vals, 4)) /
             ((len(vals) - 1) * (std_dev(vals) ** 4)))
+
 
 def num_stats(vals):
     return {
