4ccbbe1265d6205ae46c24373fb4e33aeb4177fa
Mahmoud Hashemi <makuro@gmail.com>
2012-08-25 05:24:34 -0700

    robustify input_server, also modify it for no relative imports

--- input_server.py
+++ input_server.py
@@ -1,5 +1,7 @@
+import wapiti
+
 import bottle
-from bottle import route, run, JSONPlugin
+from bottle import route, run, request, JSONPlugin
 
 from functools import partial
 better_dumps = partial(bottle.json_dumps, indent=2,
@@ -12,30 +14,43 @@ bottle.default_app().install(JSONPlugin(better_dumps))
 # hashing input code for remote execution
 # caching results
 import pkgutil
-from inputs import Input
 
 input_mods = [importer.find_module(name).load_module(name)
               for (importer, name, _) in pkgutil.walk_packages('.')
               if name.startswith('inputs.')]
+
 AVAIL_INPUTS = {}
 
 for im in input_mods:
     for m in im.__dict__.itervalues():
-        if type(m) is type and issubclass(m, Input) and m is not Input:
+        if type(m) is type and hasattr(m, 'fetch'):
             AVAIL_INPUTS[m.__name__] = m
+            AVAIL_INPUTS[m.__name__.lower()] = m
+AVAIL_INPUTS.pop('Input', None)
 
 
+@route('/<input_name>/<page_title>/')
 @route('/<input_name>/<page_title>/<page_id:int>')
 def do_input(input_name, page_title, page_id=None):
-    in_type = AVAIL_INPUTS.get(input_name)
+    in_type = AVAIL_INPUTS.get(input_name.lower())
     if in_type is None:
         raise Exception('No input found with name "' + input_name + '"')
+    if not page_title:
+        raise Exception('You must pass in a page title (and preferably a page id)')
+    if not page_id:
+        page = wapiti.get_articles(titles=page_title, parsed=False)[0]
+        page_id = page.page_id
     # TODO: optional page_id, do lookup if None
     in_obj = in_type(page_title, page_id)
     results = in_obj()
-    import pdb;pdb.set_trace()
     return results
     #return in_obj.results
+
+@route('/<input_name>/')
+def do_input_qs(input_name):
+    page_title = request.query.title
+    page_id = request.query.page_id
+    return do_input(input_name, page_title, page_id)
 
 if __name__ == '__main__':
     bottle.debug(True)
